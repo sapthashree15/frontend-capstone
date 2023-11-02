f4eb21437fc65c764f4bf2ff8cd0168bc511fa0d
sapthashree15 <kssapthashree@gmail.com>
2023-11-02 06:03:09 -0400

    changes made from theia lab environment

---
+++ src/config.js
@@ -0,0 +1,5 @@
+export const API_URL = window.location.hostname === "localhost" ? "https://sapthashreek-8080.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/" : "add your theia server side url";
+console.log(
+    "API_URL :",
+    API_URL
+);
