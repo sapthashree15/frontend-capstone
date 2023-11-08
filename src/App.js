202c4088da48a384a97369da9edc04f14e7b7a22
sapthashree15 <kssapthashree@gmail.com>
2023-11-08 00:47:31 -0500

    changes made from theia lab environment

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Login from './Components/Login/Login';
 import SignUp from './Components/Sign_Up/Sign_Up'; // Corrected import
 import Navbar from './Components/Navbar/Navbar';
 import LandingPage from './Components/Landing_Page/Landing_Page'; // Corrected import
+import InstantConsultation from './Components/InstantConsultation/InstantConsultationBooking/InstantConsultation'
 
 function App() {
   return (
@@ -12,8 +13,9 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/signup" element={<SignUp />} />
+        <Route path="/instant-consultation" element={<InstantConsultation/>} />
         <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
       </Routes>
     </BrowserRouter>
   );
